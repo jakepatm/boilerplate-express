08b9da4f3b1d72eb6f2533a73360539ec530e32b
Jacob Meyers <jakepatm@gmail.com>
2022-11-12 15:03:02 -0500

    Added dotenv require header, added if/else for message style, added .env file with global variable on styling

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 //console.log("Hello World");
-
+require('dotenv').config();
 
 let express = require('express');
 let app = express();
@@ -18,7 +18,13 @@ app.use("/public",express.static(assetPath)) */
 let absPath = __dirname + "/json";
 
 function handler (req,res) {
-	res.json({"message" : "Hello json"})
+	if(process.env.MESSAGE_STYLE == "uppercase") {
+		res.json({"message" : "HELLO JSON"});
+	}
+	else {
+		res.json({"message" : "Hello json"})
+	}
+	
 }
 
 app.get("/json",handler)
